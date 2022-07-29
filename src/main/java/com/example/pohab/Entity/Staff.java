1e2b60b067fbd9eb7efb19bcf12a7d716d3fcc0e
sunseo18 <sunseo1818@gmail.com>
2022-07-29 16:22:57 +0900

    feat: 회원 / 그룹 / 운영진 Entity 추가

---
+++ src/main/java/com/example/pohab/Entity/Staff.java
@@ -0,0 +1,46 @@
+package com.example.pohab.Entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "staff")
+public class Staff {
+    //필드
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private Long user_id;
+
+    @Column(length = 27, nullable = false)
+    private String group_id;
+
+    @Column(length = 15, nullable = false)
+    private String role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    //빌더
+    @Builder
+    public Staff(Long user_id, String group_id, String role) {
+        this.user_id = user_id;
+        this.group_id = group_id;
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/example/pohab/Entity/User.java
@@ -0,0 +1,37 @@
+package com.example.pohab.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+//@Table(name = "user")
+public class User {
+    //필드
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, length = 50, nullable = false)
+    private String email;
+
+    @Column(length = 50, nullable = false)
+    private String password;
+
+    @Column(length = 50, nullable = false)
+    private String phone;
+
+    //빌더
+    @Builder
+    public User(String email, String password, String phone) {
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+    }
+}
