777edd87d52039791b96b57e9516713b246c408f
sunseo18 <sunseo1818@gmail.com>
2022-07-29 16:22:57 +0900

    feat: 회원 / 그룹 / 운영진 Entity 추가

---
+++ src/main/java/com/example/pohab/Entity/Group.java
@@ -0,0 +1,32 @@
+package com.example.pohab.Entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "group")
+public class Group {
+    //필드
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+    @Column(length = 25, nullable = false)
+    private String name;
+
+    @Column(length = 25, nullable = false)
+    private String category;
+
+    //빌더
+    @Builder
+    public Group(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+}
