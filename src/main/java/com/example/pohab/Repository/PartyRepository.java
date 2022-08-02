0d77b4cb97b07521304903ac8bbc59dcf03e4fc8
sunseo18 <sunseo1818@gmail.com>
2022-08-02 17:37:36 +0900

    feat: party enroll(entity 수정 필요)

---
+++ src/main/java/com/example/pohab/Repository/PartyRepository.java
@@ -0,0 +1,8 @@
+package com.example.pohab.Repository;
+
+import com.example.pohab.Entity.Party;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartyRepository extends JpaRepository<Party, Integer> {
+
+}
