00dfa0a78311b50363d5ba6bd234bf44b27effc5
sunseo18 <sunseo1818@gmail.com>
2022-08-02 17:37:36 +0900

    feat: party enroll(entity 수정 필요)

---
+++ src/main/java/com/example/pohab/DTO/PartyEnrollDTO.java
@@ -0,0 +1,14 @@
+package com.example.pohab.DTO;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PartyEnrollDTO {
+    private String name;
+    private String category;
+    private Integer personnel;
+    private Integer nth;
+}
--- src/main/java/com/example/pohab/Entity/Party.java
+++ src/main/java/com/example/pohab/Entity/Party.java
@@ -3,19 +3,20 @@ package com.example.pohab.Entity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @Entity
 @Table(name = "party")
 public class Party {
     //필드
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(unique = true, nullable = false)
-    private Integer id;
+    private String id;
 
     @Column(length = 25, nullable = false)
     private String name;
