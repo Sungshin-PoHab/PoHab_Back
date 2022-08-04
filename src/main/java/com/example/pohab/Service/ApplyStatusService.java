42f16caedf862cc87e93ff86436c4ed643d2b2eb
zinzo1019 <topjoy22@naver.com>
2022-08-04 11:19:32 +0900

    feat: ApplyStatus Repository & Service 추가 -> 지원 현황 & 지원 인원 세기 함수 추가

--- src/main/java/com/example/pohab/Service/ApplyStatusService.java
+++ src/main/java/com/example/pohab/Service/ApplyStatusService.java
@@ -1,13 +1,33 @@
 package com.example.pohab.Service;
 
 import com.example.pohab.Entity.ApplyStatus;
-import com.example.pohab.Entity.Party;
+import com.example.pohab.Entity.Department;
+import com.example.pohab.Entity.User;
+import com.example.pohab.Repository.ApplyStatusRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface ApplyStatusService {
+@Service
+@RequiredArgsConstructor
+public class ApplyStatusService {
+
+    private final ApplyStatusRepository applyStatusRepository;
 
     /** 지원 현황 전체 리스트 가져오기 */
-    public List<ApplyStatus> getAllApplyStatus();
+    public List<ApplyStatus> getAllApplyStatus() {
+        return applyStatusRepository.findAll();
+    }
+
+    /** 사용자별 지원 현황 리스트 가져오기 */
+    public List<ApplyStatus> getAllApplyStatusByUser(User user) {
+        return applyStatusRepository.getAllApplyStatusByUser(user);
+    }
+
+    /** 부서별 지원 인원 세기 */
+    public int countApplyStatusByDepartment(Department department) {
+        return applyStatusRepository.countApplyStatusByDepartment(department);
+    }
 
 }
