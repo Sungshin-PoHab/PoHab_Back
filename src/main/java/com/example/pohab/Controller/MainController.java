f0eb8213656bccf6cc28068fd72c3c8be912b6cc
zinzo1019 <topjoy22@naver.com>
2022-08-06 02:29:05 +0900

    feat: 메인 페이지 api 추가

---
+++ src/main/java/com/example/pohab/Controller/MainController.java
@@ -0,0 +1,25 @@
+package com.example.pohab.Controller;
+
+import com.example.pohab.Entity.ApplyStatus;
+import com.example.pohab.Entity.Department;
+import com.example.pohab.Service.DepartmentService;
+import com.example.pohab.Service.MainDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class MainController {
+
+    private final DepartmentService departmentService;
+
+    /** 메인페이지 */
+    @GetMapping("/main")
+    public List<MainDto> main() {
+        return departmentService.entityToMainDto();
+    }
+
+}
---
+++ src/main/java/com/example/pohab/Service/DepartmentService.java
@@ -0,0 +1,65 @@
+package com.example.pohab.Service;
+
+import com.example.pohab.Dto.StepDateDto;
+import com.example.pohab.Entity.Department;
+import com.example.pohab.Entity.Step;
+import com.example.pohab.Repository.DepartmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DepartmentService {
+
+    private final DepartmentRepository departmentRepository;
+    private final StepService stepService;
+    private final ApplyStatusService applyStatusService;
+
+    /** 부서 전체 리스트 가져오기 */
+    public List<Department> getAllDepartment() {
+        return departmentRepository.findAll();
+    }
+
+    /** 기본키로 특정 부서 가져오기 */
+    public Department getDepartmentById(int id) {
+        return departmentRepository.findById(id).get();
+    }
+
+    /** department -> mainDto */
+    public List<MainDto> entityToMainDto() {
+        List<Department> allDepartment = getAllDepartment();
+        List<Step> allStep = stepService.getAllStep();
+
+        List<StepDateDto> stepDateDtos = new ArrayList<>();
+        for (Step step : allStep) {
+            StepDateDto stepDateDto = StepDateDto.builder()
+                    .step(step.getStep())
+                    .startDate(step.getStartDate())
+                    .endDate(step.getEndDate())
+                    .build();
+            stepDateDtos.add(stepDateDto);
+        }
+
+        List<MainDto> mainDtos = new ArrayList<>();
+
+        for (Department department : allDepartment) {
+            int 지원자_수 = applyStatusService.countApplyStatusByDepartment(department);
+            System.out.println("지원자 수: " + 지원자_수);
+            double 경쟁률 = 지원자_수 / (double) department.getPersonnel();
+            System.out.println("모집 인원 수: " + department.getPersonnel());
+            System.out.println("경쟁률: " + 경쟁률);
+            MainDto mainDto = MainDto.builder()
+                    .party(department.getParty().getName())
+                    .competition(경쟁률)
+                    .stepDateDtos(stepDateDtos)
+                    .build();
+            mainDtos.add(mainDto);
+        }
+
+        return mainDtos;
+    }
+
+}
