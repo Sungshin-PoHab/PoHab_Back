e6e73f84b3587797001b86cb2c5bf8fae08df28c
zinzo1019 <topjoy22@naver.com>
2022-08-06 12:55:53 +0900

    feat: 메인화면을 위한 Dto 추가 (MainDto, StepDateDto)

---
+++ src/main/java/com/example/pohab/Dto/MainDto.java
@@ -0,0 +1,17 @@
+package com.example.pohab.Dto;
+
+import com.example.pohab.Dto.StepDateDto;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MainDto {
+    private String party; // 소속명
+    private double competition; // 경쟁률
+    List<StepDateDto> stepDateDtos; // 단계 + 모집 날짜
+}
