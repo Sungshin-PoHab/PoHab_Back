79b7b6773444cef829e1c3e97d1a3b30a0019e26
zinzo1019 <topjoy22@naver.com>
2022-08-07 04:52:32 +0900

    feat: 운영진용 지원 현황 dto

--- src/main/java/com/example/pohab/DTO/ApplyStatusForStaffDto.java
+++ src/main/java/com/example/pohab/DTO/ApplyStatusForStaffDto.java
@@ -13,7 +13,7 @@ public class ApplyStatusForStaffDto {
     private String party; // 소속 + 기수
     private String department; // 부서
     private int step; // 단계
-    private int applicant; // 지원 인원
-    private int competition; // 경쟁률
+    private int applicantNum; // 지원 인원
+    private double competition; // 경쟁률
     private List<ApplicantDto> applicantDtoList; // 지원자 리스트
 }
