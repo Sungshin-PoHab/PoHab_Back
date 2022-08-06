bd69d48f307b2a00d82482eab83720170142e823
zinzo1019 <topjoy22@naver.com>
2022-08-06 12:59:28 +0900

    feat: StepRepository & StepService 생성

---
+++ src/main/java/com/example/pohab/Repository/StepRepository.java
@@ -0,0 +1,8 @@
+package com.example.pohab.Repository;
+
+import com.example.pohab.Entity.Step;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StepRepository extends JpaRepository<Step, Integer> {
+
+}
