584cad7d54669210e1ab8cf59cf6ca44e2fabec3
zinzo1019 <topjoy22@naver.com>
2022-08-06 13:00:33 +0900

    feat: ApplyStatusRepository & ApplyStatusService 생성

---
+++ src/main/java/com/example/pohab/Repository/ApplyStatusRepository.java
@@ -0,0 +1,26 @@
+package com.example.pohab.Repository;
+
+import com.example.pohab.Entity.ApplyStatus;
+import com.example.pohab.Entity.Department;
+import com.example.pohab.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ApplyStatusRepository extends JpaRepository<ApplyStatus, Integer> {
+
+    /** 특정 사용자의 지원 현황 */
+    @Query("select a from ApplyStatus a where a.user = :user")
+    List<ApplyStatus> getAllApplyStatusByUser(@Param("user") User user);
+
+    /** 특정 부서의 지원 현황 */
+    @Query("select a from ApplyStatus a where a.department = :department")
+    List<ApplyStatus> getAllApplyStatusByDepartment(@Param("department") Department department);
+
+    /** 특정 부서별 지원 인원 세기 */
+    @Query("select count(distinct a.user) from ApplyStatus a where a.department = :department")
+    int countApplyStatusByDepartment(@Param("department") Department department);
+}
