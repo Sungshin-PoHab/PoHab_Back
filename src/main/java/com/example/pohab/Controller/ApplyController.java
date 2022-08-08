1d03f6c116823c09d74bebcd4de11333823e1d98
sunseo18 <sunseo1818@gmail.com>
2022-08-09 01:23:29 +0900

    feat: 처음 회원이 동아리 지원하는 api 구현

--- src/main/java/com/example/pohab/Controller/ApplyController.java
+++ src/main/java/com/example/pohab/Controller/ApplyController.java
@@ -1,4 +1,34 @@
 package com.example.pohab.Controller;
 
+import com.example.pohab.DTO.ApplyUserForPartyDTO;
+import com.example.pohab.DTO.CreateStepDto;
+import com.example.pohab.DTO.PartyEnrollDTO;
+import com.example.pohab.Entity.ApplyStatus;
+import com.example.pohab.Entity.Step;
+import com.example.pohab.Service.ApplyStatusService;
+import com.example.pohab.Service.StepService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/apply")
 public class ApplyController {
+
+    private ApplyStatusService applyStatusService;
+
+    @Autowired
+    public ApplyController(ApplyStatusService applyStatusService) {
+        this.applyStatusService = applyStatusService;
+    }
+
+    // 모집 일정(step) 생성
+    @PostMapping("/saveStatus")
+    public ApplyStatus saveStatus(@RequestBody ApplyUserForPartyDTO applyUserForPartyDTO) {
+        return this.applyStatusService.applyUserTForarty(applyUserForPartyDTO);
+    }
 }
