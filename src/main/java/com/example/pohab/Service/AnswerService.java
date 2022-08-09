2286fe5b8a3bb755175b65fa7cfda89289214472
sunseo18 <sunseo1818@gmail.com>
2022-08-09 14:14:29 +0900

    feat: 지원서 작성 ( 처음 지원할 때 빈 답변) 틀 잡기

--- src/main/java/com/example/pohab/Service/AnswerService.java
+++ src/main/java/com/example/pohab/Service/AnswerService.java
@@ -1,2 +1,25 @@
-package com.example.pohab.Service;public class AnswerService {
+package com.example.pohab.Service;
+
+import com.example.pohab.Repository.AnswerRepository;
+import com.example.pohab.Repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AnswerService {
+    private AnswerRepository answerRepository;
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    public AnswerService(AnswerRepository answerRepository, QuestionRepository questionRepository){
+        this.answerRepository = answerRepository;
+        this.questionRepository = questionRepository;
+    }
+
+    public void saveEmptyAnswers() {
+
+    }
+
 }
