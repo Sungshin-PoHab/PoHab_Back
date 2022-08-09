34d956e73ba665658b26f0d3bc79d0e1303f59d0
zinzo1019 <topjoy22@naver.com>
2022-08-10 03:19:10 +0900

    fix: jwt에 id, name, email 한 번에 담기

--- src/main/java/com/example/pohab/Login/JWT/JwtProperties.java
+++ src/main/java/com/example/pohab/Login/JWT/JwtProperties.java
@@ -4,6 +4,8 @@ public interface JwtProperties {
     String SECRET = "PoHab"; // Signatuer를 해싱할 때 사용되는 비밀 키
     int EXPIRATION_TIME =  864000000; // 토큰 만료 기간(10일)
     String TOKEN_PREFIX = "Bearer "; // 토큰 앞에 붙는 정해진 형식 (공백 필수)
-    String HEADER_STRING = "Authorization"; // 헤더의 Authorization 항목에 토큰을 넣는다.
-    String USER_EMAIL = "userEmail"; // 헤더의 UserId 항목에 토큰을 넣는다.
+    String HEADER_STRING = "authorization"; // 헤더의 Authorization 항목에 토큰을 넣는다.
+    String USER_ID = "userId";
+    String USER_EMAIL = "userEmail";
+    String USER_NAME = "userName";
 }
