e6fa8ac13c644e00a8815a503e35af84ecf4fd2f
sunseo18 <sunseo1818@gmail.com>
2022-08-09 18:55:09 +0900

    feat: 최종 제출 api 구현

--- src/main/java/com/example/pohab/Controller/ApplyController.java
+++ src/main/java/com/example/pohab/Controller/ApplyController.java
@@ -1,12 +1,15 @@
 package com.example.pohab.Controller;
 
 import com.example.pohab.DTO.ApplyUserForPartyDTO;
+import com.example.pohab.Entity.Answer;
 import com.example.pohab.Entity.ApplyStatus;
 import com.example.pohab.Service.AnswerService;
 import com.example.pohab.Service.ApplyStatusService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -31,4 +34,11 @@ public class ApplyController {
         return applyStatus;
     }
 
+    /** 최종 지원 (지원서 제출) **/
+    @PostMapping("/{department_id}/{step_id}")
+    public ApplyStatus submitAnswer(@PathVariable("department_id") Integer department_id, @PathVariable("step_id") Integer step_id, @RequestBody List<Answer> answers){
+        this.answerService.tempSaveAnswer(answers);
+        //로그인 구현 후에 user 수정해야함
+        return this.applyStatusService.statusUpdateToSubmit(1, department_id, step_id);
+    }
 }
