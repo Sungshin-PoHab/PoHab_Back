5d266b5f08ef44c63eb62f06758bcbc473f43037
zinzo1019 <topjoy22@naver.com>
2022-08-11 14:25:28 +0900

    fix: 메인 페이지 수정에 따라 Dto 수정

--- src/main/java/com/example/pohab/Dto/MainDto.java
+++ src/main/java/com/example/pohab/Dto/MainDto.java
@@ -1,6 +1,5 @@
 package com.example.pohab.DTO;
 
-import com.example.pohab.DTO.StepDateDto;
 import lombok.*;
 
 import java.util.List;
@@ -12,6 +11,8 @@ import java.util.List;
 @NoArgsConstructor
 public class MainDto {
     private String party; // 소속명
-    private double competition; // 경쟁률
-    List<StepDateDto> stepDateDtos; // 단계 + 모집 날짜
+    private List<String> competition; // 경쟁률
+    private boolean availability; // 모집 여부
+    private List<String> department; // 모집 부서 리스트
+    private List<StepDateDto> stepDateDtos; // 단계 + 모집 날짜
 }
