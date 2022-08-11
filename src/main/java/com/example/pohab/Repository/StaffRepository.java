b54f48ab5335b6ed72f032417533f805b8fd77a2
sunseo18 <sunseo1818@gmail.com>
2022-08-11 22:04:28 +0900

    feat: 소속 생성할 때 생성자 운영진으로 등록

---
+++ src/main/java/com/example/pohab/Repository/StaffRepository.java
@@ -0,0 +1,7 @@
+package com.example.pohab.Repository;
+
+import com.example.pohab.Entity.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StaffRepository extends JpaRepository<Staff, Integer> {
+}
--- src/main/java/com/example/pohab/Service/PartyService.java
+++ src/main/java/com/example/pohab/Service/PartyService.java
@@ -2,18 +2,33 @@ package com.example.pohab.Service;
 
 import com.example.pohab.DTO.PartyEnrollDTO;
 import com.example.pohab.Entity.Party;
+import com.example.pohab.Entity.Staff;
+import com.example.pohab.Entity.User;
 import com.example.pohab.Repository.PartyRepository;
+import com.example.pohab.Repository.StaffRepository;
+import com.example.pohab.Repository.UserRepository;
+import com.netflix.discovery.converters.Auto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.Random;
 
 @Service
 public class PartyService {
-    @Autowired
-    PartyRepository partyRepository;
 
-    public Party enroll(PartyEnrollDTO partyEnrollDTO) {
+    private PartyRepository partyRepository;
+    private StaffRepository staffRepository;
+    private UserRepository userRepository;
+
+    @Autowired
+    public PartyService (PartyRepository partyRepository, StaffRepository staffRepository, UserRepository userRepository){
+        this.partyRepository = partyRepository;
+        this.staffRepository = staffRepository;
+        this.userRepository = userRepository;
+    }
+
+    public Party enroll(Integer userId, PartyEnrollDTO partyEnrollDTO) {
         Party party = new Party();
 
         String nth = partyEnrollDTO.getNth().toString();
@@ -33,7 +48,26 @@ public class PartyService {
         party.setCode(code);
         party.setName(partyEnrollDTO.getName());
 
-        partyRepository.save(party);
+        //해당 소속에 user 운영진으로 등록
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isPresent()) {
+            User realuser = user.get();
+
+            Staff staff = Staff.builder()
+                    .party(party)
+                    .role("운영진")
+                    .user(realuser)
+                    .build();
+
+            //소속 생성
+            partyRepository.save(party);
+            //소속 운영진 등록
+            staffRepository.save(staff);
+
+        } else {
+
+        }
         return party;
     }
 }
