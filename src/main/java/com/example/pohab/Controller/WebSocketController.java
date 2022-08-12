cae9407622b4d31f0ee5ee03c71a43f67f54a19e
eunjungL <dldmswjd322@gmail.com>
2022-08-12 15:51:13 +0900

    feat: stomp socket feat add

---
+++ src/main/java/com/example/pohab/Controller/WebSocketController.java
@@ -0,0 +1,18 @@
+package com.example.pohab.Controller;
+
+import com.example.pohab.DTO.ChatDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class WebSocketController {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/chat")
+    public void sendMessage(ChatDto chatDto) {
+        simpMessagingTemplate.convertAndSend("/sub/chat/", chatDto);
+    }
+}
