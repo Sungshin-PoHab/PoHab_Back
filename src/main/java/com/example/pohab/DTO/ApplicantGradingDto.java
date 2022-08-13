bad44b0bc5212c3d91aead03ffdf6d45a628fa98
zinzo1019 <topjoy22@naver.com>
2022-08-13 21:01:17 +0900

    fix: 결과 통보 dto 수정

--- src/main/java/com/example/pohab/DTO/ApplicantGradingDto.java
+++ src/main/java/com/example/pohab/DTO/ApplicantGradingDto.java
@@ -1,6 +1,5 @@
 package com.example.pohab.DTO;
 
-import com.example.pohab.Enum.IsPass;
 import lombok.*;
 
 @Data
@@ -9,6 +8,7 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApplicantGradingDto {
+    private int applyId; // applyStatus 아이디
     private String name; // 지원자 이름
     private double score; // 점수
     private double highScore; // 최고 점수
